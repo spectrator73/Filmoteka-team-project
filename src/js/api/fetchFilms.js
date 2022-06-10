5e430b977882ab526c56180560b6bf60fa9940f6
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-06-10 21:07:50 +0300

    films api added

---
+++ src/js/api/fetchFilms.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const KEY = '?api_key=2ddded2d287329b6efbf335a6f8f3bd4';
+
+async function fetchFilms(url) {
+  try {
+    const response = await axios.get(url);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// Посилання на документацію для запиту на список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці:
+export function fetchTrending() {
+  return fetchFilms(`${BASE_URL}/trending/all/day${KEY}`)
+    .then(data => data.results)
+    .catch(error => console.log(error));
+}
+// Посилання на документацію для запиту фільму за ключовим словом на головній сторінці:
+export function searchMovies(movie) {
+  return fetchFilms(`${BASE_URL}/search/movie${KEY}&query=${movie}`)
+    .then(data => data.results)
+    .catch(error => console.log(error));
+}
+// Посилання на документацію для запиту повної інформації про кінофільм для сторінки кінофільму:
+export function getOneMovieDetails(movieId) {
+  return fetchFilms(`${BASE_URL}/movie/${movieId}${KEY}`);
+}
+
+// Приклад застосування
+// document.querySelector('.yourSelector').addEventListener('click', () => {
+//   return searchMovies('batman').then(r => console.log(r));
+// });
