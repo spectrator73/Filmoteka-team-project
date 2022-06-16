e77ce4c3d8912b910c0e1ec87babc46b1ca4cd83
chelydra-ua <lipara.shep@gmail.com>
2022-06-16 10:21:30 +0300

    dark-light theme added

---
+++ src/js/dark-mode/dark-mode.js
@@ -0,0 +1,26 @@
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  document.querySelector('main').classList.add('darkmode');
+  localStorage.setItem('darkMode', 'enabled');
+};
+
+const disableDarkMode = () => {
+  document.querySelector('main').classList.remove('darkmode');
+  localStorage.setItem('darkMode', null);
+};
+
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode');
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
