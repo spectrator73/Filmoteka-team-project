6d007dd167b32f94df977161d5f3a3ca1ad46ec0
NickDurnev <92080056+NickDurnev@users.noreply.github.com>
2022-06-16 21:46:41 +0300

    Add active state to library buttons

--- src/library.js
+++ src/library.js
@@ -1,4 +1,5 @@
 import './js/back-to-top-btn/back-to-top-btn';
+import './js/add-active-state-btn/libraryButtons';
 
 // import './js/api/trendingFilmRender';
 // import './js/api/searchFilmsByNameRender';
