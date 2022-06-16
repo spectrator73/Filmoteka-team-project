9f341b783588a017a2cd1002ccfcaa2c425f3f46
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-06-16 20:19:09 +0300

    hbs id added

---
+++ src/library.js
@@ -0,0 +1,6 @@
+import './js/back-to-top-btn/back-to-top-btn';
+
+// import './js/api/trendingFilmRender';
+// import './js/api/searchFilmsByNameRender';
+// import './js/dark-mode/dark-mode';
+// import './js/modal/modal';
