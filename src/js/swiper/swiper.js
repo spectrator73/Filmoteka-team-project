8c49c06f11c5a0291dbac93a5aff7afe660ded83
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-06-17 19:41:23 +0300

    fixed slider and placeholder

--- src/js/swiper/swiper.js
+++ src/js/swiper/swiper.js
@@ -5,47 +5,44 @@ const swiperContainer = document.querySelector('.swiper-wrapper');
 
 async function trendingMoviesRender() {
   const data = await filmsAPI.fetchTrending();
-	renderMovieCardsSlider(data)
+  renderMovieCardsSlider(data);
 }
 
 function renderMovieCardsSlider(results) {
-	
-	const markup = results.map(({ poster_path, id }) => {
-		
-		let imagePoster = `https://image.tmdb.org/t/p/w500${poster_path}`;		
-		
-		return `<div class="swiper-slide">
+  const markup = results
+    .map(({ poster_path, id }) => {
+      let imagePoster = `https://image.tmdb.org/t/p/w500${poster_path}`;
+
+      return `<div class="swiper-slide">
 				<img src="${imagePoster}" alt="poster" class="slide-poster" data-id="${id}"/>
 			</div>`;
-    }).join(''); 
-  swiperContainer.insertAdjacentHTML("beforeend", markup);
+    })
+    .join('');
+  swiperContainer.insertAdjacentHTML('beforeend', markup);
 }
-trendingMoviesRender()
+trendingMoviesRender();
 
 const swiper = new Swiper('.mySwiper', {
-    modules: [Navigation, Autoplay],
-    spaceBetween: 5,
-    breakpointsBase: 'container',
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
+  modules: [Navigation, Autoplay],
+  spaceBetween: 5,
+  breakpointsBase: 'container',
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    410: {
+      slidesPerView: 2,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    600: {
+      slidesPerView: 6,
     },
-    breakpoints: {
-        210: {
-            slidesPerView: 2,
-        },
-        600: {
-            slidesPerView: 6,
-        },
-        768: {
-            slidesPerView: 8,
-        },
+    768: {
+      slidesPerView: 8,
     },
-    });
-
-
-
+  },
+});
