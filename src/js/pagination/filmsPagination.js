9bb2dd712bf1610149a7e804161e91c027163689
NickDurnev <92080056+NickDurnev@users.noreply.github.com>
2022-06-19 19:34:20 +0300

    Fix pagination error

--- src/js/pagination/filmsPagination.js
+++ src/js/pagination/filmsPagination.js
@@ -12,11 +12,13 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 let pageNumber = 1;
 let totalPages = 20;
 let searchQuery = null;
+let prevSearchQuery = null;
 
 const refs = {
   linkList: document.querySelector('.pages-list'),
   btnNext: document.querySelector('button[data-action="next"]'),
   btnPrev: document.querySelector('button[data-action="prev"]'),
+  navList: document.querySelector('.page-navigation'),
 };
 
 refs.linkList.addEventListener('click', filmsPagination);
@@ -45,9 +47,9 @@ async function getTotalPages() {
   if (!data.results.length) {
     return Notify.failure('Sorry, there are no films matching your search query. Please try again.');
   }
-  
   const { total_pages, results } = data;
   totalPages = total_pages;
+  prevSearchQuery = searchQuery;
   fisrtrButtonsRender(1, totalPages);
   
   renderMarkup(results);
@@ -62,11 +64,25 @@ async function fetchController() {
   if (!searchQuery) {
     trendingMoviesRender();
   }
+
   else {
     try {
       const data = await filmsAPI.searchMovies(searchQuery, pageNumber);
       const { results } = data;
-      renderMarkup(results);
+      if (!results.length) {
+        searchQuery = prevSearchQuery;
+      }
+      if (!results.length && !prevSearchQuery) {
+        trendingMoviesRender();
+        return
+      }
+      if (!results.length && prevSearchQuery) {
+        const data = await filmsAPI.searchMovies(prevSearchQuery, pageNumber);
+        const { results } = data;
+        renderMarkup(results);
+        return
+      }
+        renderMarkup(results);
     }
     catch (error) {
     console.log(error.message);
