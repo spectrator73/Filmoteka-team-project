0a96b43b196778c6f1c970a0b984de2a72ce33fb
Vladimir Pavlov <91782115+Popeye80@users.noreply.github.com>
2022-06-20 17:54:01 +0300

    loader

--- src/library.js
+++ src/library.js
@@ -3,7 +3,7 @@ import './js/auth-firebase/auth-main';
 import './js/back-to-top-btn/back-to-top-btn';
 import './js/add-active-state-btn/libraryButtons';
 import './js/pagination/libraryFilmsPagination';
-
+import './js/our-team-modal/our-team-modal';
 // import './js/api/trendingFilmRender';
 // import './js/api/searchFilmsByNameRender';
 import './js/modal/modal-library';
